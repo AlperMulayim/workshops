faf655288df45e18270a16cb96f720841a643edb
alpermulayim <alper.mulayim48@gmail.com>
2022-04-27 14:20:18 +0300

    OAuth2 projects created initialized

---
+++ oauth2/oauth2-be/src/main/java/com/alper/oauth2be/Oauth2BeApplication.java
@@ -0,0 +1,13 @@
+package com.alper.oauth2be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2BeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2BeApplication.class, args);
+	}
+
+}
---
+++ oauth2/oauth2-be/src/test/java/com/alper/oauth2be/Oauth2BeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.oauth2be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2BeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
