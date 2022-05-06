2c2fc85add0f72a0353cd05a99d5c0a94a9028f8
alpermulayim <alper.mulayim48@gmail.com>
2022-05-06 13:14:01 +0300

    Unit Test Workshop init.

---
+++ unittest-workshop/src/main/java/com/alper/unittestworkshop/UnittestWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.alper.unittestworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnittestWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UnittestWorkshopApplication.class, args);
+	}
+
+}
