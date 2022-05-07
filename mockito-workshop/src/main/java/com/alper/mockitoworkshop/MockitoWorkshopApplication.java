63d7efc640852f2313987a03140bf81d90ec0254
alpermulayim <alper.mulayim48@gmail.com>
2022-05-07 12:07:16 +0300

    Mockito Workshop project init.

---
+++ mockito-workshop/src/main/java/com/alper/mockitoworkshop/MockitoWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.alper.mockitoworkshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoWorkshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MockitoWorkshopApplication.class, args);
+	}
+
+}
