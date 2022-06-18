98338317b73f5d9e7f5b252e83c600c2be00b25f
alpermulayim <alper.mulayim48@gmail.com>
2022-06-18 14:48:33 +0300

    Otp Auth Provider implemented for check authenticate

--- multi-auth/src/main/java/com/alper/security/multiauth/repositories/OtpRepository.java
+++ multi-auth/src/main/java/com/alper/security/multiauth/repositories/OtpRepository.java
@@ -3,5 +3,8 @@ package com.alper.security.multiauth.repositories;
 import com.alper.security.multiauth.entities.Otp;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface OtpRepository  extends JpaRepository<Otp,Integer> {
+    Optional<Otp> findOtpByUsername(String username);
 }
---
+++ multi-auth/src/main/java/com/alper/security/multiauth/security/providers/OtpAuthProvider.java
@@ -0,0 +1,36 @@
+package com.alper.security.multiauth.security.providers;
+
+import com.alper.security.multiauth.entities.Otp;
+import com.alper.security.multiauth.repositories.OtpRepository;
+import com.alper.security.multiauth.security.authentications.OtpAuth;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+import java.util.List;
+import java.util.Optional;
+
+public class OtpAuthProvider  implements AuthenticationProvider {
+
+    @Autowired
+    private OtpRepository otpRepository;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username = authentication.getName();
+        String otpStr = (String) authentication.getCredentials();
+
+        Optional<Otp> optionalOtp = otpRepository.findOtpByUsername(username);
+        Otp otp = optionalOtp.orElseThrow(()-> new BadCredentialsException("OTP PROVIDER NOT VALIDATE PASS"));
+        return  new OtpAuth(username,otp, List.of(()->"read"));
+
+    }
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return OtpAuthProvider.class.equals(authentication);
+    }
+}
