a5d1afe4e6761d7f00cd9e8b7dbf0aaca826e289
alpermulayim <alper.mulayim48@gmail.com>
2022-06-18 13:35:41 +0300

    multi-auth provider project added

---
+++ multi-auth/src/main/java/com/alper/security/multiauth/MultiAuthApplication.java
@@ -0,0 +1,13 @@
+package com.alper.security.multiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultiAuthApplication.class, args);
+	}
+
+}
---
+++ multi-auth/src/test/java/com/alper/security/multiauth/MultiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.security.multiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
