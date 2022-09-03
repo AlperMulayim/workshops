6a719c59b8b31364a249f8aab98a8031c4fd268b
alpermulayim <alper.mulayim48@gmail.com>
2022-09-03 10:21:48 +0300

    websocket starter app

---
+++ websockettutor/src/main/java/com/alper/websockettutor/WebsockettutorApplication.java
@@ -0,0 +1,13 @@
+package com.alper.websockettutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsockettutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebsockettutorApplication.class, args);
+	}
+
+}
---
+++ websockettutor/src/test/java/com/alper/websockettutor/WebsockettutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.websockettutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsockettutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
