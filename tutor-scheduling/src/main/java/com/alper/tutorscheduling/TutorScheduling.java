f483909693a9d89c581d3e871cf51f2b6b472025
alpermulayim <alper.mulayim48@gmail.com>
2022-09-14 16:07:23 +0300

    scheduling notes added

--- tutor-scheduling/src/main/java/com/alper/tutorscheduling/TutorScheduling.java
+++ tutor-scheduling/src/main/java/com/alper/tutorscheduling/TutorScheduling.java
@@ -1,9 +1,41 @@
 package com.alper.tutorscheduling;
 
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
-@Component
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.UUID;
+
+@Service
 public class TutorScheduling {
 
+    @Scheduled(fixedDelay = 2000)
+    public void  fixedDelayCompute(){
+        UUID uuid = UUID.randomUUID();
+        System.err.println("Fixed Delay--> " + uuid.toString());
 
+        System.err.println("DELAY-> " +LocalDateTime.now().toLocalTime());
+    }
+
+    @Scheduled(fixedRate = 3000)
+    @Async
+    public void  fixedRate(){
+        UUID uuid = UUID.randomUUID();
+        System.err.println("Fixed Rate--> " + uuid.toString());
+
+        System.err.println("RATE-> " + LocalDateTime.now().toLocalTime());
+    }
+
+    //https://crontab.cronhub.io/ GET THE CRON
+    //https://docs.oracle.com/cd/E12058_01/doc/doc.1014/e12030/cron_expressions.htm
+    @Scheduled(cron = "0 15 16 * * ?")
+    public void  cronJob(){
+        UUID uuid = UUID.randomUUID();
+        System.err.println("CRON Rate--> " + uuid.toString());
+
+        System.err.println("CRON-> " + LocalDateTime.now().toLocalTime());
+    }
 }
--- tutor-scheduling/src/main/java/com/alper/tutorscheduling/TutorSchedulingApplication.java
+++ tutor-scheduling/src/main/java/com/alper/tutorscheduling/TutorSchedulingApplication.java
@@ -2,8 +2,10 @@ package com.alper.tutorscheduling;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class TutorSchedulingApplication {
 
 	public static void main(String[] args) {
