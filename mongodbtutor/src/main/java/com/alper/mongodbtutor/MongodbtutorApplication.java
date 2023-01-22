2cddb1cf58ca67dc65c39d4f2d55eec7859fa160
alpermulayim <alper.mulayim48@gmail.com>
2023-01-22 16:53:34 +0300

    Mongodb spring boot project init.

---
+++ mongodbtutor/src/main/java/com/alper/mongodbtutor/MongodbtutorApplication.java
@@ -0,0 +1,13 @@
+package com.alper.mongodbtutor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongodbtutorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongodbtutorApplication.class, args);
+	}
+
+}
---
+++ mongodbtutor/src/test/java/com/alper/mongodbtutor/MongodbtutorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.mongodbtutor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongodbtutorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
