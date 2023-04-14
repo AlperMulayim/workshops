d9517be7f7f2acffc6c72b3ff6e7473a3e596bc3
alpermulayim <alper.mulayim48@gmail.com>
2023-04-14 20:55:53 +0300

    svgapi init files added

---
+++ svgapi/src/main/java/com/alper/svgapi/SvgapiApplication.java
@@ -0,0 +1,13 @@
+package com.alper.svgapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SvgapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SvgapiApplication.class, args);
+	}
+
+}
---
+++ svgapi/src/test/java/com/alper/svgapi/SvgapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alper.svgapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SvgapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
